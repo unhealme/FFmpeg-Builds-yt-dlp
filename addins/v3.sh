43b6b38e150c33530175cf7fa92fb6005ad1e1b7
Mickel Edward <o2tongpethuk@gmail.com>
2024-04-09 22:35:19 +0700

    Update v3.sh

--- addins/v3.sh
+++ addins/v3.sh
@@ -2,5 +2,5 @@
 FF_CONFIGURE+=" --cpu=x86-64-v3"
 
 ffbuild_dockeraddin() {
-    to_df 'ENV CFLAGS="$CFLAGS -march=x86-64-v3 -mtune=znver2" CXXFLAGS="$CXXFLAGS -march=x86-64-v3 -mtune=znver2" LDFLAGS="$LDFLAGS"'
+    to_df 'ENV CFLAGS="$CFLAGS -march=x86-64-v3 -mtune=znver2" CXXFLAGS="$CXXFLAGS -march=x86-64-v3 -mtune=znver2"'
 }
